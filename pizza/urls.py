e841339692410be8a75d5f8bf1faf766359c2a66
Sergey Manzhak <manzhak.com@gmail.com>
2018-08-16 16:13:20 +0300

    Add url

--- pizza/urls.py
+++ pizza/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from store.views import (hello_world, 
+                        index)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', hello_world),
+    path('', index, name='index'),
 ]

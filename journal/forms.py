4011509b1e410a9ccd09b56e2c7a15619ba874ad
Sergey Manzhak <manzhak.ru@gmail.com>
2018-08-19 16:00:21 +0300

    Add valid for textarea

--- journal/forms.py
+++ journal/forms.py
@@ -4,17 +4,22 @@ from django.core.exceptions import ValidationError
 
 class WriteOutForm(forms.Form):
     write_out = forms.CharField(widget=forms.Textarea(attrs={'rows': 5, 'cols': 100}), label='Выписка')
-    dispatcher = forms.CharField(label='Диспетчер')
+    dispatcher = forms.CharField(label='Диспетчер', max_length=50)
 
     def clean_dispatcher(self):
         dispatcher = self.cleaned_data['dispatcher']
-        if len(dispatcher) < 5:
+        if len(dispatcher) < 3:
             raise ValidationError('Too short')
         return dispatcher
 
     def clean(self):
         if self.cleaned_data.get('dispatcher', None) == 'Миша':
             raise ValidationError('No!')
+        return self.cleaned_data
+
+    def clean_write_out(self):
+        if 'fuck' in self.cleaned_data.get('write_out', None):
+            raise ValidationError('Fuck you')
         return self.cleaned_data
 
 

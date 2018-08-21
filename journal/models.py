0bfe18cfb4ae53b90df2aa856d987f669201b025
Sergey Manzhak <manzhak.com@gmail.com>
2018-08-21 07:44:27 +0300

    Start add model

--- journal/models.py
+++ journal/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+
+class WriteOut(models.Model):
+    data_created = models.DateField(default=datetime.now())
+    write_out = models.CharField(widget=forms.Te)
